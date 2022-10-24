9665fc701b1e3e873a67387743c21bfe72d1e234
Dillon Kim <dillonkim@Dillons-Air.attlocal.net>
2022-10-24 11:19:20 -0700

    add homepage and questionpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,64 @@
 var quiz = document.getElementById("quiz");
+
+var questions = [
+    {
+        title: 'Question 1: ;alskdjf',
+        answers: [
+            {
+                answer: 1,
+                correct: false
+            },
+            {
+                answer: 100,
+                correct: false
+            },
+            {
+                answer: 123,
+                correct: true
+            }
+            
+        ]
+    },
+    {
+        title: 'Question 2: ;alskdjf',
+        answers: [
+            {
+                answer: 234,
+                correct: false
+            },
+            {
+                answer: 432,
+                correct: false
+            },
+            {
+                answer: 123,
+                correct: true
+            }
+            
+        ]
+    },
+    {
+        title: 'Question 3: ;alskdjf',
+        answers: [
+            {
+                answer: 234,
+                correct: false
+            },
+            {
+                answer: 432,
+                correct: false
+            },
+            {
+                answer: 123,
+                correct: true
+            }
+            
+        ]
+    }
+]
+
+var currentQ = 0;
+
 function homepage(){
     // var quizTitle = document.createElement('p');
     // quizTitle.textContent = 'My Quiz'
@@ -13,22 +73,35 @@ function homepage(){
     document
     .getElementById('startQuiz')
     .addEventListener(
-        'click', function () {
-            questionPage()
+        'click', 
+        function () {
+            questionPage(questions[currentQ])
         }
     )
 }
 
-function questionPage(){
+function questionPage(question){
     quiz.innerHTML = /* html */ `
-    <p>
-        Question 1
-    </p>
-    <ul>
-        <li><button>Option 1</button></li>
-    </ul>
-    <button>Start Quiz</button>
-`
+        <p>
+            ${question.title}
+        </p>
+        <ul>
+            <li><button id="answerOne">${question.answers[0].answer}</button></li>
+            <li><button id="answerTwo">${question.answers[1].answer}</button></li>
+            <li><button id="answerThree">${question.answers[2].answer}</button></li>
+        </ul>
+    `
+
+    document
+    .getElementById('answerOne')
+    .addEventListener(
+        'click', 
+        function () {
+            currentQ++
+            questionPage(questions[currentQ])
+        }
+    )
+
 }
 
 homepage();
